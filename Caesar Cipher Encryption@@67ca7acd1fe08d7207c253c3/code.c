8e59c32d27c11f27bba411a79991acccf370fe93
Prince <princeyadavhansi2@gmail.com>
2025-04-05 15:55:32 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,25 +1,17 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 char *caesarCipher(char str[], int n, char encrypted[]) {
     int i;
-
-    // Loop through each character in the input string
-    for(i = 0; i < strlen(str); i++) {
-        // Handle uppercase letters
-        if(str[i] >= 'A' && str[i] <= 'Z') {
-            encrypted[i] = ((str[i] - 'A' + n) % 26 + 26) % 26 + 'A'; // Wrap around the alphabet
-        }
-        // Handle lowercase letters
-        else if(str[i] >= 'a' && str[i] <= 'z') {
-            encrypted[i] = ((str[i] - 'a' + n) % 26 + 26) % 26 + 'a'; // Wrap around the alphabet
+    for (i = 0; i < strlen(str); i++) {
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+            encrypted[i] = ((str[i] - 'A' + n) % 26 + 26) % 26 + 'A';
+        } else if (str[i] >= 'a' && str[i] <= 'z') {
+            encrypted[i] = ((str[i] - 'a' + n) % 26 + 26) % 26 + 'a';
         } else {
-            // Non-alphabetical characters remain the same
             encrypted[i] = str[i];
         }
     }
-
-    // Null-terminate the encrypted string
     encrypted[i] = '\0';
     return encrypted;
 }
@@ -27,11 +19,8 @@ char *caesarCipher(char str[], int n, char encrypted[]) {
 int main() {
     char str[] = "Hello, World!";
     int shift = 3;
-    char encrypted[100]; // Make sure this array is large enough to hold the encrypted text
-
+    char encrypted[100];
     printf("Original: %s\n", str);
-    caesarCipher(str, shift, encrypted);
-    printf("Encrypted: %s\n", encrypted);
-
+    printf("Encrypted: %s\n", caesarCipher(str, shift, encrypted));
     return 0;
 }
