9db74ed438f4536764285dbe22535b9afce2d2e0
Prince <princeyadavhansi2@gmail.com>
2025-04-07 16:57:03 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,26 +1,15 @@
-#include <stdio.h>
-#include <string.h>
+#include <ctype.h>
 
-char *caesarCipher(char str[], int n, char encrypted[]) {
-    int i;
-    for (i = 0; i < strlen(str); i++) {
-        if (str[i] >= 'A' && str[i] <= 'Z') {
-            encrypted[i] = ((str[i] - 'A' + n) % 26 + 26) % 26 + 'A';
-        } else if (str[i] >= 'a' && str[i] <= 'z') {
-            encrypted[i] = ((str[i] - 'a' + n) % 26 + 26) % 26 + 'a';
+void caesarCipher(char message[], int shift, char encrypted[]) {
+    int i = 0;
+    while (message[i] != '\0') {
+        if (isalpha(message[i])) {
+            char base = isupper(message[i]) ? 'A' : 'a';
+            encrypted[i] = (message[i] - base + shift) % 26 + base;
         } else {
-            encrypted[i] = str[i];
+            encrypted[i] = message[i];
         }
+        i++;
     }
     encrypted[i] = '\0';
-    return encrypted;
-}
-
-int main() {
-    char str[] = "Hello, World!";
-    int shift = 3;
-    char encrypted[100];
-    printf("Original: %s\n", str);
-    printf("Encrypted: %s\n", caesarCipher(str, shift, encrypted));
-    return 0;
 }
